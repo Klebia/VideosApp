cc5c6e95e89b20e35e4715e470c9f2a8370d19ac
Klebia Thome <klebiaprof@gmail.com>
2021-06-05 21:10:06 -0300

    Criamos a página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'mortal-kombat',
-    loadChildren: () => import('./filmes/mortal-kombat/mortal-kombat.module').then( m => m.MortalKombatPageModule)
-  },
-  {
-    path: 'cuella',
-    loadChildren: () => import('./filmes/cuella/cuella.module').then( m => m.CuellaPageModule)
+    redirectTo: 'login',
+    pathMatch: 'full'
+
   },
 
   {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
     path: 'dados-filme',
     loadChildren: () => import('./dados-filme/dados-filme.module').then( m => m.DadosFilmePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
